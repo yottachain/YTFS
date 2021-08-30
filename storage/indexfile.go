baf83e6c25f8f7e48c6f940e02a45e503fd34c5d
xiaojianming <xiaojianming@yottachain.io>
2021-08-30 17:04:13 +0800

    debug

--- storage/indexfile.go
+++ storage/indexfile.go
@@ -574,7 +574,8 @@ func initializeIndexStorage(store Storage, config *opt.Options) (*ydcommon.Heade
 		ytfsSize += storageOption.StorageVolume
 	}
 
-	if m > 2048 || m <512 {
+	//  M * expendRatioM  that is (2048 * 1.2) >= m >= (512 * 1.2)
+	if m > 2457 || m <614 {
 		err := fmt.Errorf("IndexTableCols(M) not suitable,M=",m)
 		fmt.Println("[error] ",err.Error())
 		return nil, err
