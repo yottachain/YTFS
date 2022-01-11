547d62cd17b9366fed45129502398f2627f4c189
zhangzhengyan <zzy_19831106@163.com>
2022-01-11 15:59:59 +0800

    update

--- opt/property.go
+++ opt/property.go
@@ -2,7 +2,7 @@ package opt
 
 // properties for internal use.
 var (
-	DebugPrint             = false // Debug print
-	IgnoreStorageHeaderErr = true  // Ignore storage header mismatch with config.
-	expendRatioM           = 1.2   // Expending ratio of M (col of index table)
+	DebugPrint             = true // Debug print
+	IgnoreStorageHeaderErr = true // Ignore storage header mismatch with config.
+	expendRatioM           = 1.2  // Expending ratio of M (col of index table)
 )
